46ea6679d3546f5237a97de159b37425b3a4b524
RezaSR <RezaSR@localhost>
2018-12-22 15:43:34 +0000

    Add CRUD using gii

---
+++ views/user/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\User */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nickname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'balance')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'authkey')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
